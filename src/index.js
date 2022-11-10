77a9c70faf0ce59a0ba12456d7586e3673209c81
Git21478 <ViktorWinn@mail.ru>
2022-11-10 07:16:38 +0300

    36

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './index.css';
-import rerenderEntireTree from './render'
+import App from './App';
+import {addPost, subscribe, updateNewPostText} from './redux/state'
 import state from './redux/state'
 
 
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+let rerenderEntireTree = (state) => {
+root.render(
+  <React.StrictMode>
+    <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+  </React.StrictMode>
+);
+}
+rerenderEntireTree(state)
 
-rerenderEntireTree(state);
+subscribe(rerenderEntireTree);
