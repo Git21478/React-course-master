4e2d0e1094b5d8db90eea5769aaffaf27722efa0
Git21478 <ViktorWinn@mail.ru>
2022-11-14 09:38:25 +0300

    57

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { connect } from 'react-redux';
 import axios from 'axios';
-import Users from './Users';
-import loader from '../../assets/images/loader.gif'
+import Users from './Users'
+import Preloader from '../common/Preloader/Preloader';
 
 
 class UsersContainer extends React.Component {
@@ -28,7 +28,7 @@ class UsersContainer extends React.Component {
 
     render() {
         return <>
-                {this.props.isFetching ? <img src={loader}/> : null}
+                {this.props.isFetching ? <Preloader/> : null}
                 <Users  totalUsersCount={this.props.totalUsersCount}
                         pageSize={this.props.pageSize}
                         onPageChanged={this.onPageChanged}
