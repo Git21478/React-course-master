63f1d1b23e03e223a115ef99ec01b71ce83e1acb
Git21478 <ViktorWinn@mail.ru>
2022-11-15 17:33:26 +0300

    62

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,7 +2,7 @@ import s from './Users.module.css'
 import React from 'react'
 import userPhoto from './../../assets/images/user.png'
 import { NavLink } from 'react-router-dom'
-
+import axios from 'axios'
 
 let Users = (props) => {
 
@@ -31,8 +31,40 @@ let Users = (props) => {
                         </div>
                         <div>
                             {u.followed
-                                ? <button onClick={() => {props.unfollow(u.id)}}>Unfollow</button>
-                                : <button onClick={() => {props.follow(u.id)}}>Follow</button>}
+                                ? <button onClick={() => {
+
+                                    axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
+                                        withCredentials: true,
+                                        headers: {
+                                            "API-KEY": "62d36086-8cf0-4417-a821-026cd8b27b32"
+                                        }                                  
+                                })
+                                    .then(response => {
+                                        if (response.data.resultCode == 0) {
+                                            props.unfollow(u.id)
+                                        }
+                                    })
+
+                                    
+                                
+                                }}>Unfollow</button>
+                                : <button onClick={() => {
+                                    axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
+                                        withCredentials: true,
+                                        headers: {
+                                            "API-KEY": "62d36086-8cf0-4417-a821-026cd8b27b32"
+                                        }                                     
+                                })
+                                    .then(response => {
+                                        if (response.data.resultCode == 0) {
+                                            props.follow(u.id)
+                                        }
+                
+        })
+
+                                    
+                                
+                                }}>Follow</button>}
                         </div>
                     </span>
                     <span>
